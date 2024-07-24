1247cd9ebbd07adcf403e92f9cd79d8eccf6527d
Brian Halpin <brian.t.halpin@gmail.com>
2024-07-24 00:17:31 -0400

    Add user and subscription table create statements

---
+++ sql/user.sql
@@ -0,0 +1,17 @@
+SELECT * FROM content_management_system.users;CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `company_name` varchar(255) DEFAULT NULL,
+  `stripe_customer_id` varchar(255) DEFAULT NULL,
+  `is_admin` tinyint(1) DEFAULT NULL,
+  `has_upload_permissions` tinyint(1) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `is_pending` tinyint(1) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `last_login_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
