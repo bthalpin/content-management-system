95b9024b0efc4690f1a68cba0b64dcaaa066288f
Brian Halpin <brian.t.halpin@gmail.com>
2024-07-26 21:33:37 -0400

    Add webhook to handle stripe payments

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,33 +1,38 @@
 import { createCheckout, createCustomer } from '@/lib/stripe';
 import { updateUser } from '@/lib/user';
 import { NextRequest } from 'next/server';
+import { NextApiRequest } from 'next';
+import { buffer } from 'micro'
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET
 
-export async function POST(request: NextRequest) {
-    const sig = request.headers.get('stripe-signature');
-
-    let event;
-    
-    if (!sig) {
-        return Response.json(`Webhook Error: No Signature`, {status: 400})
-    }
-
+export async function POST(request: NextApiRequest) {
     try {
-
-        event = stripe.webhooks.constructEvent((await request.json()).body, sig, endpointSecret!);
         
+        const sig = request.headers['stripe-signature'];
+
+        let event = request.body;
+
+        const buf = await buffer(request)
+        
+        if (!sig) {
+            return Response.json(`Webhook Error: No Signature`, {status: 400})
+        }
+
+
+        event = stripe.webhooks.constructEvent(buf, sig, endpointSecret!);
+        
+        
+        // Handle the event
+        console.log(`Unhandled event type ${event.type}`);
+    
+    
+        return Response.json('')
     } catch (err) {
         Response.json(`Webhook Error: ${err}`, {status: 400})
         return;
     }
-  
-    // Handle the event
-    console.log(`Unhandled event type ${event.type}`);
-  
-
-	return Response.json('')
 }
 
