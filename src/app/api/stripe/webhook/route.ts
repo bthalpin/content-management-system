007377316cbf218574046177cb02dfa26738198d
Brian Halpin <brian.t.halpin@gmail.com>
2024-07-26 21:43:50 -0400

    Add logs to track errors on webhook

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -9,19 +9,20 @@ const endpointSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET
 
 export async function POST(request: NextRequest) {
     try {
-        
+        console.log(endpointSecret, 'ARRIVED')
         const sig = request.headers.get('stripe-signature') as string;
-
+console.log('SIG',sig)
         const payload = await request.text();
-
+console.log(payload, 'PAYLOAD')
         
         if (!sig) {
+            console.log('RETURNING NO SIG')
             return Response.json(`Webhook Error: No Signature`, {status: 400})
         }
 
-
+console.log('BEFORE EVENT')
         const event = stripe.webhooks.constructEvent(payload, sig, endpointSecret!);
-        
+        console.log('AFTER EVENT', event)
         
         // Handle the event
         console.log(`Unhandled event type ${event.type}`);
@@ -29,6 +30,7 @@ export async function POST(request: NextRequest) {
     
         return Response.json('')
     } catch (err) {
+        console.error(err, 'ERROR FROM CATCH')
         Response.json(`Webhook Error: ${err}`, {status: 400})
         return;
     }
